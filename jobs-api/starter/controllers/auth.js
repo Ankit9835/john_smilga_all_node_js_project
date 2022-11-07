687763d9a58059f6d76058ffe54892ef14302148
Ankit9835 <ankydchrismatic@gmail.com>
2022-11-07 22:03:09 +0530

    jobs api token created

--- jobs-api/starter/controllers/auth.js
+++ jobs-api/starter/controllers/auth.js
@@ -1,9 +1,19 @@
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError} = require('../errors')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const login = async (req,res) => {
     res.send('login user')
 }
 
 const register =  async (req,res) => {
-    res.send('register user')
+    const user = await User.create({
+       ...req.body
+    })
+    const token = jwt.sign({userId:user._id,name:user.name}, 'jwtSecret', {expiresIn:'30d',})
+    res.status(StatusCodes.CREATED).json({user: {name: user.name}, token})
 }
 
 module.exports = {login,register}
