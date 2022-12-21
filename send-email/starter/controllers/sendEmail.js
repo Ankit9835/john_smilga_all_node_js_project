ded9b816f19637be727ec13a6cc5ecf932fa3c64
Ankit9835 <ankydchrismatic@gmail.com>
2022-12-21 11:12:19 +0530

    new commit

--- send-email/starter/controllers/sendEmail.js
+++ send-email/starter/controllers/sendEmail.js
@@ -9,8 +9,8 @@ const sendEmailEthernal = async (req,res) => {
         host: 'smtp.ethereal.email',
         port: 587,
         auth: {
-          user: 'quinn.johnson@ethereal.email',
-          pass: '5VUSD7jeuTPBmKbahn'
+            user: 'orin.kutch@ethereal.email',
+            pass: 'K4pV98FYvHHpDz7A9C'
         }
     });
 
